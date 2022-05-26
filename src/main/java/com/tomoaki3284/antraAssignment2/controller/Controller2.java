c0df2fe518e9f116542c162e0dfee696666cd23f
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-05-26 13:01:46 -0400

    implemented service interface

--- src/main/java/com/tomoaki3284/antraAssignment2/controller/Controller2.java
+++ src/main/java/com/tomoaki3284/antraAssignment2/controller/Controller2.java
@@ -1,13 +1,13 @@
 package com.tomoaki3284.antraAssignment2.controller;
 
 import com.tomoaki3284.antraAssignment2.service.EmployeesService;
+import com.tomoaki3284.antraAssignment2.service.EmployeesServiceImpl;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
